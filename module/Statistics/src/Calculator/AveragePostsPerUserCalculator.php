9b404bebec37312f86e6bc1539b69b3a4f19d29d
Andrey Zavarzin <mail@autosaratov.ru>
2022-06-30 15:08:11 +0300

    Add average posts per user per month calculator

---
+++ module/Statistics/src/Calculator/AveragePostsPerUserCalculator.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Statistics\Calculator;
+
+use SocialPost\Dto\SocialPostTo;
+use Statistics\Dto\StatisticsTo;
+use UnexpectedValueException;
+
+class AveragePostsPerUserCalculator extends AbstractCalculator
+{
+    protected const UNITS = 'posts';
+
+    private array $postsByUser = [];
+
+    /**
+     * @inheritDoc
+     */
+    protected function doAccumulate(SocialPostTo $postTo): void
+    {
+        $authorId = $postTo->getAuthorId();
+        if (null === $authorId) {
+            throw new UnexpectedValueException('author_id cannot be null');
+        }
+
+        $this->postsByUser[$authorId] = $this->postsByUser[$authorId] ?? 0;
+        $this->postsByUser[$authorId]++;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function doCalculate(): StatisticsTo
+    {
+        $value = count($this->postsByUser) > 0
+            ? (array_sum($this->postsByUser) / count($this->postsByUser))
+            : 0;
+
+        return (new StatisticsTo())->setValue(round($value,2));
+    }
+}
---
+++ tests/unit/module/Statistics/src/Calculator/AveragePostsPerUserCalculatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\unit\module\Statistics\src\Calculator;
+
+use Statistics\Calculator\AveragePostsPerUserCalculator;
+use PHPUnit\Framework\TestCase;
+
+class AveragePostsPerUserCalculatorTest extends TestCase
+{
+
+}
